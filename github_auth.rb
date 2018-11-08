b5c08d81d251656d2a290444bb32f8cb18c15795
Nick Bartlett <nbartlett7@gmail.com>
2018-11-08 03:59:31 -0500

    attempt script fix read from home directory

--- github_auth.rb
+++ github_auth.rb
@@ -12,5 +12,5 @@ print "Github Password (nothing will be displayed):"
 password  = STDIN.noecho(&:gets).strip
 github = Github.new(:login => user_name, :password => password)
 github.users.keys.create("title" => "Bootcamp", 
-	"key"=> File.open("~/.ssh/id_rsa.pub").read)
+	"key"=> File.open(ENV['HOME']+"/.ssh/id_rsa.pub").read)
 puts "\nok!"
